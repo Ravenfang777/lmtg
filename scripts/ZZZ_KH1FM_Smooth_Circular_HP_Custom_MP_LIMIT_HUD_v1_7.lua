bb33377905a0d3388f78b275ba0b7f6ccac74543
Raven <Greyvira777@gmail.com>
2026-07-31 06:13:36 -0400

    Update ZZZ_KH1FM_Smooth_Circular_HP_Custom_MP_LIMIT_HUD_v1_7.lua

--- scripts/ZZZ_KH1FM_Smooth_Circular_HP_Custom_MP_LIMIT_HUD_v1_7.lua
+++ scripts/ZZZ_KH1FM_Smooth_Circular_HP_Custom_MP_LIMIT_HUD_v1_7.lua
@@ -178,7 +178,7 @@ local CONFIG = {
         },
         {
             ENABLE = true,
-            X = 216, Y = 128,
+            X = 216, Y = 126,
             WIDTH = 16, HEIGHT = 8,
             COLOR = 0x80000000,
         },
