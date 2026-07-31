0203e6ebaf86f1a7bce22cbbe612bf7b89ea9c57
Raven <Greyvira777@gmail.com>
2026-07-31 04:35:26 -0400

    Update ZZZ_KH1FM_Smooth_Circular_HP_Custom_MP_LIMIT_HUD_v1_6.lua

--- scripts/ZZZ_KH1FM_Smooth_Circular_HP_Custom_MP_LIMIT_HUD_v1_6.lua
+++ scripts/ZZZ_KH1FM_Smooth_Circular_HP_Custom_MP_LIMIT_HUD_v1_6.lua
@@ -236,7 +236,7 @@ local CONFIG = {
     ORIGIN = {
         X = 75,
         Y = 112,
-        SCALE = 0.54,
+        SCALE = 0.40,
     },
 
     -- Exact doubled-pixel bounds reconstructed from the supplied 0.50-scale
