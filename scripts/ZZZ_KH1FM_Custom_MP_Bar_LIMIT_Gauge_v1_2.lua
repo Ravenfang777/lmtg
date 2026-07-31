686ea355124e178e6d10e29142a95d1307563a90
Raven <Greyvira777@gmail.com>
2026-07-31 00:44:22 -0400

    Update ZZZ_KH1FM_Custom_MP_Bar_LIMIT_Gauge_v1_2.lua

--- scripts/ZZZ_KH1FM_Custom_MP_Bar_LIMIT_Gauge_v1_2.lua
+++ scripts/ZZZ_KH1FM_Custom_MP_Bar_LIMIT_Gauge_v1_2.lua
@@ -200,9 +200,9 @@ local CONFIG = {
         CYCLE_SECONDS = 1.00,
         COLOR_STEPS = 30,
         OUTLINE_START_COLOR = 0x80FFEE00, -- RGB 0,238,255
-        OUTLINE_PEAK_COLOR = 0x800000B5,  -- RGB 181,0,0
+        OUTLINE_PEAK_COLOR = 0x8000005E,  -- RGB 94,0,0
         TEXT_START_COLOR = 0x80FFEE00,    -- RGB 0,238,255
-        TEXT_PEAK_COLOR = 0x800000B5,     -- RGB 181,0,0
+        TEXT_PEAK_COLOR = 0x8000005E,     -- RGB 94,0,0
     },
 
     -- Kept as a named setting so all threshold logic stays explicit.
